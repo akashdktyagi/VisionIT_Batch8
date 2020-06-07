31b179335f78a91941226efeec3fd0fa7f844f95
rahul10101 <rahul.bhujbal10@gmail.com>
2020-06-07 12:38:26 +0530

    updated RestTempUI

--- Batch8CucumberSeleniumAPI/src/test/java/runner/RunTempUI.java
+++ Batch8CucumberSeleniumAPI/src/test/java/runner/RunTempUI.java
@@ -8,16 +8,16 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-		features="classpath:features/ui/SearchMultipleProducts.feature",
+		features="classpath:features/ui",
 		glue="stepdefs.ui",
-		tags="@TC008",
+		tags="@TC0008",
 		plugin = {"pretty",
 				"html:target/html/",
 				"json:target/json/file.json",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 		},
 		strict=false,
-		dryRun=false
+		dryRun=true
 
 		)
 public class RunTempUI {
