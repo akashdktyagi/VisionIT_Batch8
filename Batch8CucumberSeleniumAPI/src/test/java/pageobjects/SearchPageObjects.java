34982b34ea5c9b63ffeafddd60de8e3d04f9e260
PritiJodh18 <pritisjodh@gmail.com>
2020-06-07 10:24:53 +0530

    Made changes in feature file and some changes in SearchpageObject

--- Batch8CucumberSeleniumAPI/src/test/java/pageobjects/SearchPageObjects.java
+++ Batch8CucumberSeleniumAPI/src/test/java/pageobjects/SearchPageObjects.java
@@ -12,56 +12,50 @@ import utils.ui.Interact;
 
 public class SearchPageObjects extends Interact {
 
-Scenario scn;
-	
+	Scenario scn;
+
 	private By product_list = By.xpath("//span[@class='a-size-medium a-color-base a-text-normal']");
 	private By product_list_all = By.xpath("//div[@class='s-result-list s-search-results sg-row']//span[@class='a-size-medium a-color-base a-text-normal']");
-	//TC0028
-		private By Not_clickble_previous = By.xpath("//ul[@class='a-pagination']//li[text()='Previous']");
-		private By Previous= By.xpath("//ul[@class='a-pagination']//a[text()='Previous']");
-		private By next = By.xpath("//ul[@class='a-pagination']//a[text()='Next']");
-		private By Not_clickble_next = By.xpath("//ul[@class='a-pagination']//li[text()='Next']");
-	
+	private By PreviousBtn = By.xpath("//li[text()='Previous']");
+	private By ClickPreviousBtn= By.xpath("//a[text()='Previous']");
+	private By ClickNextBtn = By.xpath("//a[text()='Next']");
+	private By NextBtn = By.xpath("//li[text()='Next']");
+
 	public SearchPageObjects(WebDriver driver,Scenario s) {
 		setDriver(driver);
 		this.scn = s;
 	}
-	
 	public String ClickOnProductLink() {
 		List<WebElement> list = getListOfWebElements(product_list_all);
 		clickElement(list.get(0));
 		scn.write("Clicked on First Product Link");
 		return list.get(0).getText();
 	}
-	public void Check_Previous_btn() {
-  		validateElementIsDisplayed(Not_clickble_previous);
-  		}
-  		public void click_To_Lastpage() {
-  			
-  			 String nextbtn=getDriver().findElement(next).getAttribute("tagName");
-  			 while(!nextbtn.contains("//li")) {
-  		     clickElement(next);
-  		}
-  			 }
-  		
-  		public void Check_next_btn() {
-  		validateElementIsDisplayed(Not_clickble_next);
-  		}
-  		
-  	    public void Previous_To_Firstpage() {
-  	    	String nextbtn1=getDriver().findElement(Previous).getAttribute("class");
-  			 while(!nextbtn1.contains("a-disabled")) {
-  	         clickElement(Previous);	
-  			 }
-  	    }
-	
+	public boolean CheckPreviousBtn() {
+		return validateElementIsDisplayed(PreviousBtn);
+	}
+	public void ClickOnlastPageLink() {
+		while(CheckNextBtn())
+		{
+			clickElement(ClickNextBtn);
+		}
+	}
+	public boolean CheckNextBtn() {
+		return validateElementIsDisplayed(NextBtn);
+	}
+	public void ClickOnFirstPageLink() {
+		String nextbtn1=getDriver().findElement(ClickPreviousBtn).getAttribute("class");
+		while(!nextbtn1.contains("a-disabled")) {
+			clickElement(ClickPreviousBtn);	
+		}
+	}
 	public String ClickOnProductLink(int productIndex) {
 		List<WebElement> list = getListOfWebElements(product_list_all);
 		clickElement(list.get(productIndex));
 		scn.write("Clicked on Product id: " + productIndex + " Product Link");
 		return list.get(productIndex).getText();
 	}
-	
+
 	public String ClickOnProductLink(String productTextContains) {
 		List<WebElement> list = getListOfWebElements(product_list_all);
 		boolean flag=false;
@@ -74,20 +68,20 @@ Scenario scn;
 				break;
 			}
 		}
-		
+
 		if (flag) {
 			scn.write("Clicked on Product containing text as: " + productTextContains + "");
 		}else {
 			scn.write("Unable to click on Product containing text as: " + productTextContains + " No product found with mentioned text");
 			Assert.fail("Unable to click on Product containing text as: " + productTextContains + " No product found with mentioned text");
 		}
-		
+
 		return list.get(counter).getText();
 
 	}
-	
+
 	public void ValidateProductList(String productName) {
-	
+
 		List<WebElement> list_products = getListOfWebElements(product_list);
 		for (int i=0;i<list_products.size();i++) {
 			if (list_products.get(i).getText().toLowerCase().contains(productName.toLowerCase())) {
@@ -96,6 +90,6 @@ Scenario scn;
 				Assert.fail("Product not correctly displayed in the search result. Product at index: " + (i+1));
 			}
 		}
-		
+
 	}
 }
--- Batch8CucumberSeleniumAPI/src/test/resources/features/ui/Search.feature
+++ Batch8CucumberSeleniumAPI/src/test/resources/features/ui/Search.feature
@@ -27,8 +27,8 @@ Feature: Search functionality
 	  Scenario: Verify Pagination On Product Search Page
 		Given I have browser opened and url is navigated
 		When I search for product as "Dell"
-		Then I check when user is on the first page not able to click on the previous button
-		Then User is able click to the next button when user has already navigated to any other page or on the first page
-	  Then User Should not be able to click on the Next button when the user is on the last page
-	  Then User is able to the previous button when the user has already navigated to any other page or on the last page
+    Then Pagination panel with products of first page are displayed by default
+    And Previous, Page Links and Next link are working as expected
+    	 
 	 
+	
